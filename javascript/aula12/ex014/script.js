fc7a68c9abff65081c3d4798639be09cce6ed1e2
felipevet <119354385+felipevet@users.noreply.github.com>
2024-03-10 16:41:19 -0300

    ex014 termino

---
+++ javascript/aula12/ex014/script.js
@@ -0,0 +1,22 @@
+function carregar() {
+    var hr = new Date()
+    var hora = hr.getHours()
+    var min = hr.getMinutes()
+    var back = document.querySelector('section#corpo')
+    var img = document.querySelector('#img')
+    var msg = document.querySelector('div#msg')
+    
+    if (hora < 12 && min > 0){
+        back.style.background = 'yellow'
+        msg.innerHTML = `Bom dia!!! Agora são ${hora}:${min}Hs`
+        img.src = `assets/manha.jpg`
+    } else if (hora > 12 && min > 0) {
+        back.style.background = 'orange'
+        msg.innerHTML = `Boa tarde!!! Agora são ${hora}:${min}Hs`
+        img.src = `assets/tarde.jpg`
+    } else {
+        back.style.background = 'gray'
+        msg.innerHTML = `Boa noite!!! Agora são ${hora}:${min}Hs`
+        img.src = `assets/noite.jpg`
+    }
+}
