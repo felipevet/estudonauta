78387363571429917c9e532c84022aeb5417c6b7
felipevet <119354385+felipevet@users.noreply.github.com>
2024-03-10 12:43:17 -0300

    fim do projeto

---
+++ javascript/aula11/ex008.js
@@ -0,0 +1,10 @@
+var vel = 65.0
+console.log(`Sua velocidade foi de ${vel}Km/h`)
+
+if (vel > 60) {
+    console.log(`Você ultrapassou o limite de velocidade permitido, portanto será MULTADO!`)
+} else {
+    console.log(`Sua velocidade é de ${vel}Km/h. Você está dentro do limite permitido!`)
+}
+
+console.log(`Dirija com cuidado, utilize sempre o cinto de segurança!!`)
