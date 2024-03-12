3358b9d533a75f7947f0c22d1734292e22a0d631
felipevet <119354385+felipevet@users.noreply.github.com>
2024-03-12 06:33:40 -0300

    Create ambiente.js

---
+++ javascript/aula13/ambiente.js
@@ -0,0 +1,15 @@
+var c = 1
+
+// while (c <= 6) {
+//     console.log(`passo --> ${c}`)
+//     c++
+// }
+
+// do {
+//     console.log(`passo --> ${c}`)
+//     c++
+// }while(c <=0)
+
+for(var i = 0; i <= 10; i++) {
+    console.log(`incremento --> ${i}`)
+}
