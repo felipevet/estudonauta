dfe6cb993291e84cd2c9033f2fabe1b98578631c
felipevet <119354385+felipevet@users.noreply.github.com>
2024-03-12 12:38:44 -0300

    att

--- javascript/aula13/ex016/script.js
+++ javascript/aula13/ex016/script.js
@@ -3,8 +3,23 @@ function contar() {
     var txtEnd = document.querySelector('input#txt-end')
     var txtPass = document.querySelector('input#txt-pass')
     var resp = document.querySelector('div#res')
-    for(var i = Number(txtStart.value); i <= Number(txtEnd.value); i += Number(txtPass.value)) {
-        resp.innerHTML += `Contando<br> ${i} &#128073; `
+    var parTxt = document.querySelector('p#txt')
+    
+    if(txtStart.value.length == 0 || txtEnd.value.lenght == 0 || txtPass.value.lenght == 0) {
+        parTxt.innerHTML = `Impossível contar!!!`
+    } else if(Number(txtPass.value) == 0 ) {
+        window.alert(`Passo inválido! Considerando PASSO = 1 !`)
+        parTxt.innerHTML = `Contando ... `
+        for(var i = Number(txtStart.value); i <= Number(txtEnd.value); i++) {
+            resp.innerHTML += ` ${i} &#128073; `
+        }
+        resp.innerHTML += ` &#9873;`
+    } else {
+        parTxt.innerHTML = `Contando ... `
+        for(var i = Number(txtStart.value); i <= Number(txtEnd.value); i += Number(txtPass.value)) {
+            resp.innerHTML += ` ${i} &#128073; `
+        }
+        resp.innerHTML += ` &#9873;`
     }
-    //resp.innerHTML = `${txtStart.value}, ${txtEnd.value}, ${txtPass.value}`
+    
 }
